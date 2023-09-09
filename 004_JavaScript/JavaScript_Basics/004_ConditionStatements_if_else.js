81bf5df87d399fa50663a3678b1877e9ca510258
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-09-09 18:05:01 +0530

    Create 004_ConditionStatements_if_else.js

---
+++ 004_JavaScript/JavaScript_Basics/004_ConditionStatements_if_else.js
@@ -0,0 +1,29 @@
+//In JavaScript, the if...else statement is used to control the flow of your code based on a condition. It allows you to execute different blocks of code 
+//depending on whether the condition is true or false. 
+
+
+
+// If Else 
+// #1 - Single If Else
+let number = 10;
+if(number > 10)
+{
+    console.log("Number is greater than 10");
+}
+else 
+{
+    console.log("Number is lesser or equal to 10");
+}
+
+// #2 - Multiple if else 
+if(number < 10)
+{
+    console.log("Number is lesser than 10");
+}
+else if(number > 10)
+{
+    console.log("Number is greater than 10");   
+}
+else{
+    console.log("Number is equal to 10");
+}
