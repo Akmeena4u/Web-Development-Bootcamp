a821804173869abe2c7a25fefa01d4c7e0520864
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-09-19 22:07:46 +0530

    Create 004_UsingDOM_FindingHTMLObjects.js

---
+++ 004_JavaScript/005_JavaScript-DOM/004_UsingDOM_FindingHTMLObjects.js
@@ -0,0 +1,38 @@
+//The Document Object Model (DOM) provides access to various HTML objects and object collections
+
+//#1-Document: The top-level object representing the entire HTML document.
+var documentObject = document;
+
+
+//#2-document.anchors:- Represents a collection of all anchor (<a>) elements in the document.
+var anchors = document.anchors;
+
+//#3- document.body:  - Represents the <body> element of the document.
+var bodyElement = document.body;
+
+//#4-document.documentElement: - Represents the root <html> element of the document.
+var htmlElement = document.documentElement;
+
+//#5- document.embeds: - Represents a collection of embedded elements, typically used for multimedia
+var embeds = document.embeds;
+
+//#6-document.forms: - Represents a collection of all HTML forms (<form>) in the document.
+var forms = document.forms;
+
+//#7-document.head: - Represents the <head> element of the document, which contains metadata and external resources.
+var headElement = document.head;
+
+
+//#8-document.images: - Represents a collection of all <img> elements in the document
+var images = document.images;
+
+//#9-document.links: - Represents a collection of all anchor (<a>) elements that have the href attribute in the document.
+var links = document.links;
+
+
+//#10- document.scripts: Represents a collection of all <script> elements in the document.
+var scripts = document.scripts;
+
+
+//#11-document.title: - Represents the title of the document as specified in the <title> element.
+var documentTitle = document.title;
