1e7a7efacf019ee6a4e298a0bf6f04abf6da183c
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-09-19 21:42:16 +0530

    Create 002_DOMBasics.js

---
+++ 004_JavaScript/005_JavaScript-DOM/002_DOMBasics.js
@@ -0,0 +1,18 @@
+/*********************************************************------JS DOM----************************************************************************
+
+JS Engine converts all HTML code of a website into a js object , that represents the structure and content of web documents
+
+With the Document object model, JavaScript gets all the power it needs to create dynamic HTML:
+
+JavaScript can change all the HTML elements in the page
+JavaScript can change all the HTML attributes in the page
+JavaScript can change all the CSS styles in the page
+JavaScript can remove existing HTML elements and attributes
+JavaScript can add new HTML elements and attributes
+JavaScript can react to all existing HTML events in the page
+JavaScript can create new HTML events in the page*/
+
+// Access the document object
+var myDocument = document;
+console.log(myDocument);
+console.log(myDocument.body);
