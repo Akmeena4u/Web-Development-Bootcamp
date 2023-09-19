474862ea89ac9b5c06d783b3b98b6567a101c2a3
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-09-19 21:45:55 +0530

    Create 003_BOMBasics.js

---
+++ 004_JavaScript/005_JavaScript-DOM/003_BOMBasics.js
@@ -0,0 +1,7 @@
+/***********************************************************-------------------JS BOM-------------*******************************************************
+
+In JavaScript, BOM stands for Browser Object Model. The Browser Object Model is a set of objects and APIs provided by web browsers to interact with and control
+various aspects of the browser itself, as opposed to the Document Object Model (DOM), which deals with the structure and content of web documents.*/
+
+
+
