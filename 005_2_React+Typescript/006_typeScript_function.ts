b959c1b884545cd3bc6f9b2dc484a41d63cbe978
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-10-30 17:58:38 +0530

    Create 006_typeScript_function.ts

---
+++ 005_2_React+Typescript/006_typeScript_function.ts
@@ -0,0 +1,56 @@
+//TypeScript has a specific syntax for typing function parameters and return values.
+
+/*-------------------------------------------------------------Function Declaration:----------------------------------------------------------------------------
+In TypeScript, you can declare functions using the function keyword, similar to JavaScript. You can specify the parameter types and the return type of a function  */
+
+  function add(x: number, y: number): number {
+  return x + y;
+}
+
+//If no parameter type is defined, TypeScript will default to using any
+
+
+/*----------------------------------------------------------Optional parameters ----------------------------------------------------------------------------------
+Optional Parameter : By default TypeScript will assume all parameters are required, but they can be explicitly marked as optional.  */
+
+function greet(name: string, age?: number): void {
+  console.log(`Hello, ${name}!`);
+  if (age !== undefined) {
+    console.log(`You are ${age} years old.`);
+  }
+}
+
+// The 'age' parameter is optional, so you can call the function with or without it.
+greet("Alice");         // Output: Hello, Alice!
+greet("Bob", 25);       // Output: Hello, Bob! You are 25 years old.
+
+//In the greet function, the age parameter is marked as optional by adding ? after its name. 
+
+
+
+/*----------------------------------------------------------Default parameter---------------------------------------------------------------------------------------
+we  can also provide default values for optional parameters. If a value is not provided when calling the function, the default value will be used. */
+
+function greetWithDefault(name: string, age: number = 30): void {
+  console.log(`Hello, ${name}! You are ${age} years old.`);
+}
+
+greetWithDefault("Charlie"); // Output: Hello, Charlie! You are 30 years old.
+greetWithDefault("David", 28); // Output: Hello, David! You are 28 years old.
+
+
+
+/*--------------------------------------------------------Rest parameter---------------------------------------------------------------------------------------------
+  Rest parameters in TypeScript allow you to represent an indefinite number of function arguments as an array. This feature is particularly useful when you want to 
+  write functions that can accept a variable number of arguments. 
+
+  Declaration :- To declare rest parameters, you use the ... syntax before the parameter name in the function's parameter list. The rest parameter must be an array type.  
+  */
+
+  function gatherInfo(name: string, ...details: string[]): void {
+  console.log(`Name: ${name}`);
+  console.log("Additional details:");
+  details.forEach(detail => console.log(detail));
+}
+
+gatherInfo("Alice", "Age: 25", "Location: New York", "Occupation: Developer");
