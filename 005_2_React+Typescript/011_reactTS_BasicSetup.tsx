70c7595abbf8dd2fd1f37267badcc461e1164bdd
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-10-30 19:29:57 +0530

    Create 011_reactTS_BasicSetup.tsx

---
+++ 005_2_React+Typescript/011_reactTS_BasicSetup.tsx
@@ -0,0 +1,12 @@
+/*
+
+in your React components, you can use TypeScript to define types for props, state, and function parameters. For example:
+
+Environmental setup steps :--
+1. install node 
+2.Create a React Project: 
+     npx create-react-app my-app --template typescript    //my-app is project name 
+     cd my-app  //navigate to project
+
+     */
+
