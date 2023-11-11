d31dac7022218fdd03b95f4d742768d86c8b4ed6
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-11-11 10:48:29 +0530

    Update 006_nodejs_OsModule.js

--- 005_Node.js/006_nodejs_OsModule.js
+++ 005_Node.js/006_nodejs_OsModule.js
@@ -1,3 +1,12 @@
+
+/*
+
+The os module in Node.js provides a way to interact with the operating system. It offers various methods to retrieve information about the operating system,
+such as CPU architecture, memory usage, and platform details.
+
+*/
+
+
 const os = require('os');
 
 console.log(os.freemem())
@@ -6,3 +15,26 @@ console.log(os.hostname())
 console.log(os.platform())
 console.log(os.release())
 console.log(os.type())
+
+
+
+//To run system commands from Node.js and store the output in a text file, you can use the child_process module. 
+// index.js
+const { exec } = require('child_process');
+const fs = require('fs');
+
+// Run the ls (or dir on Windows) command
+exec('ls', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+
+  // Store the output in a text file
+  fs.writeFileSync('output.txt', stdout);
+
+  console.log('Command Output:', stdout);
+});
+
+
+//This will execute the ls command (or dir on Windows), store the output in output.txt, and log the output to the console.
