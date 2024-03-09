3cf8ce380b5d2c8ba98fbc464c0af40dd382529b
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2024-03-09 17:02:06 +0530

    Update 007_callbackFunction.js

--- 004_JavaScript/007_JavaScript_EventLoop/007_callbackFunction.js
+++ 004_JavaScript/007_JavaScript_EventLoop/007_callbackFunction.js
@@ -1,11 +1,12 @@
-/*
-In JavaScript, a callback function is a function that is passed as an argument to another function and is executed after the completion of a specific task. 
-Callbacks are a fundamental concept in JavaScript, especially in asynchronous programming, and they are commonly used to handle events, perform asynchronous operations, 
-and manage the flow of control in the code.
 
-*/
 
-// Example 1: Simple Callback
+# Callback Functions in JavaScript
+
+In JavaScript, a callback function is a function that is passed as an argument to another function and is executed after the completion of a specific task. Callbacks are a fundamental concept in JavaScript, especially in asynchronous programming, and they are commonly used to handle events, perform asynchronous operations, and manage the flow of control in the code.
+
+## Example 1: Simple Callback
+
+```javascript
 function doSomethingAsync(callback) {
   setTimeout(function () {
     console.log("Task completed!");
@@ -18,17 +19,13 @@ function onComplete() {
 }
 
 doSomethingAsync(onComplete);
+```
 
+In this example, `doSomethingAsync` is a function that simulates an asynchronous task using `setTimeout`. It takes a callback function (`onComplete`) as an argument. Once the asynchronous task is complete, it invokes the provided callback.
 
-/*
-In this example, doSomethingAsync is a function that simulates an asynchronous task using setTimeout. It takes a callback function (onComplete) as an argument.
-Once the asynchronous task is complete, it invokes the provided callback.
-*/
+## Example 2: Callback with Fetch API
 
-
-//Callbacks are commonly used in scenarios like handling asynchronous operations, event handling, and working with APIs. Here's another example using the fetch API:
-
-// Example 2: Callback with Fetch API
+```javascript
 function fetchData(url, callback) {
   fetch(url)
     .then((response) => response.json())
@@ -45,3 +42,9 @@ function handleData(error, data) {
 }
 
 fetchData("https://jsonplaceholder.typicode.com/todos/1", handleData);
+```
+
+In this example, `fetchData` is a function that uses the Fetch API to make an asynchronous request. It takes a URL and a callback function (`handleData`). The callback is executed after fetching the data, handling both success and error cases.
+
+Callbacks are commonly used in scenarios like handling asynchronous operations, event handling, and working with APIs. They provide a way to manage the asynchronous flow of code and handle the results or errors once a task is completed.
+```
